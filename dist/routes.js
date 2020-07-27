c864d7da748f49f0ce33c7087de15f905f63d18d
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2020-07-26 21:34:21 -0300

    Aprendendo transformar o arquivo ts para jv

--- dist/routes.js
+++ dist/routes.js
@@ -1,7 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helooword = void 0;
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
 function helooword(req, res) {
+    var user = CreateUser_1.default({
+        email: 'carlosteixeira@hotmail.com',
+        password: '123456',
+        techs: ['typescript', 'react', 'react-native', { title: 'JavaScript', experience: 1000 }],
+    });
+    console.log(user.email);
     return res.json({ message: 'hello word' });
 }
 exports.helooword = helooword;
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,12 @@
 import { Request, Response,  } from 'express'
+import createUser from './services/CreateUser'
 
 export function helooword(req:Request, res:Response) {
+    const user = createUser({
+        email: 'carlosteixeira@hotmail.com',
+        password: '123456',
+        techs: [ 'typescript', 'react', 'react-native', {title: 'JavaScript', experience: 1000}],
+    })
+    console.log(user.email)
     return res.json({message: 'hello word'})
 }
